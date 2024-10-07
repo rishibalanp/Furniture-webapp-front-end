f9cbb376da1b783506362ff545ae5b5165287a61
Rishibalan Panneerselvam <rishibalanp@gmail.com>
2024-10-07 14:22:42 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),  // Provide animations for Angular Material
+    ...appConfig.providers
+  ]
+}).catch(err => console.error(err));
